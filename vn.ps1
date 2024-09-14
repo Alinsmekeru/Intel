e270a3925a5b52c1c74b27ec36e860f914cb6bbb
Alinsmekeru <113843071+Alinsmekeru@users.noreply.github.com>
2024-09-14 16:27:59 +0300

    Update vn.ps1

--- vn.ps1
+++ vn.ps1
@@ -3,7 +3,7 @@ Invoke-WebRequest -Uri https://www.tightvnc.com/download/2.8.81/tightvnc-2.8.81-
 
 msiexec /i "tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart ADDLOCAL="Server,Viewer" VIEWER_ASSOCIATE_VNC_EXTENSION=1 SERVER_REGISTER_AS_SERVICE=1 SERVER_ADD_FIREWALL_EXCEPTION=1 VIEWER_ADD_FIREWALL_EXCEPTION=1 SERVER_ALLOW_SAS=1 SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD=1243 SET_USECONTROLAUTHENTICATION=1 VALUE_OF_USECONTROLAUTHENTICATION=1 SET_CONTROLPASSWORD=1 VALUE_OF_CONTROLPASSWORD=1243
 
-
+Start-Sleep -Seconds 3
 
 # Set the loopback key to allow connections
 $loopbackKeyPath = "HKLM:\SOFTWARE\TightVNC\Server"
@@ -17,7 +17,7 @@ Set-ItemProperty -Path $loopbackKeyPath -Name $loopbackValueName -Value $loopbac
 Restart-Service -Name "TightVNC Server"
 
 
-
+Start-Sleep -Seconds 2
 
 # Set the registry key to disable hiding of desktop wallpaper
 $registryPath = "HKLM:\Software\TightVNC\Server"
